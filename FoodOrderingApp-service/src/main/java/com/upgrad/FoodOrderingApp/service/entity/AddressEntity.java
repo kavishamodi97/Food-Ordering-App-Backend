f8a8e277c46c93aa53b4663aaa86f9ab5118bcc1
kavishamodi97 <kavishamodi971@gmail.com>
2021-07-06 15:57:29 +0530

    commit with Created AddressEntity

---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/AddressEntity.java
@@ -0,0 +1,128 @@
+package com.upgrad.FoodOrderingApp.service.entity;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "address")
+public class AddressEntity implements Serializable {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "uuid", unique = true)
+    @Size(max = 200)
+    @NotNull
+    private String uuid;
+
+    @Column(name = "flat_buil_number")
+    @Size(max = 255)
+    private String flatBuilNo;
+
+    @Column(name = "locality")
+    @Size(max = 255)
+    private String locality;
+
+    @Column(name = "city")
+    @Size(max = 30)
+    private String city;
+
+    @Size(max = 30)
+    @Column(name = "pincode")
+    private String pincode;
+
+    @Column(name = "active")
+    private Integer active;
+
+    public AddressEntity() {
+    }
+
+    public AddressEntity(String uuid, String flatBuilNo, String locality, String city, String pincode, Integer active) {
+        this.uuid = uuid;
+        this.flatBuilNo = flatBuilNo;
+        this.locality = locality;
+        this.city = city;
+        this.pincode = pincode;
+        this.active = active;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getFlatBuilNo() {
+        return flatBuilNo;
+    }
+
+    public void setFlatBuilNo(String flatBuilNo) {
+        this.flatBuilNo = flatBuilNo;
+    }
+
+    public String getLocality() {
+        return locality;
+    }
+
+    public void setLocality(String locality) {
+        this.locality = locality;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPincode() {
+        return pincode;
+    }
+
+    public void setPincode(String pincode) {
+        this.pincode = pincode;
+    }
+
+    public Integer getActive() {
+        return active;
+    }
+
+    public void setActive(Integer active) {
+        this.active = active;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return new EqualsBuilder().append(this, obj).isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(this).hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
+}
